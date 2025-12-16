f6149e6cc49c90c586c027d34621f722836d0c2a
22071028-NTHA <22071028@vnu.edu.vn>
2025-12-16 15:43:03 +0700

    Update login.php

--- mid/login.php
+++ mid/login.php
@@ -1,83 +1,101 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <head>
-    <title>User login and Registration</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Login</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Font Awesome để dùng icon con mắt -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 
 <body>
-<h0>Welcome!</h0>
     <div class="container">
         <div class="login-box">
-            <div class="row">
-               <div class="col-md-6 login-left">
-                   <h2>Login Here</h2>
-                   <form action="validation.php" method="post">
+            <div class="row justify-content-center">
+                <div class="col-md-8 login-left">
+                    <h2>Login</h2>
 
-                   <div class="form-group">
-                       <label>Username</label>
-                       <label>
-                        <input type="text" name="user" class="form-control"
-                        value="<?php if(isset($_COOKIE['username'])) echo $_COOKIE['username']; ?>">
-                       </label>
-                   </div>
+                    <form action="validation.php" method="post">
 
-                   <div class="form-group">
-                       <label>Password</label>
-                       <label>
-                           <input type="password" name="password" class="form-control"
-                           value="<?php if(isset($_COOKIE['password'])) echo $_COOKIE['password']; ?>">
-                       </label>
-                   </div>
-
-                   <!-- Show / Hide Password -->
-                   <input type="checkbox" onclick="togglePassword()"> Show Password
-                   <br><br>
-
-                   <button type="submit" class="btn btn-primary">Login</button>
-
-                   <div class="form-group">
-                        <label>
-                            <input type="checkbox" name="remember"
-                               <?php if(isset($_COOKIE['username'])) echo "checked"; ?>>
-                            Remember me
-                        </label>
-                    </div>
-
-                   </form>
-               </div>
-
-                <div class="col-md-6 login-right">
-                    <h2>Registration Here</h2>
-                    <form action="registration.php" method="post">
-                     <div class="form-group">
-                         <label>Username</label>
-                         <input type="text" name="user" required>
+                        <!-- Username -->
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input
+                                type="text"
+                                name="user"
+                                class="form-control"
+                                value="<?php if(isset($_COOKIE['username'])) echo $_COOKIE['username']; ?>"
+                                required
+                            >
                         </div>
+
+                        <!-- Password with eye icon -->
                         <div class="form-group">
                             <label>Password</label>
-                            <input type="password" name="password" required>
+                            <div class="input-group">
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    class="form-control"
+                                    value="<?php if(isset($_COOKIE['password'])) echo $_COOKIE['password']; ?>"
+                                    required
+                                >
+                                <div class="input-group-append">
+                                    <span class="input-group-text" onclick="togglePassword()" style="cursor:pointer;">
+                                        <i id="eye-icon" class="fa fa-eye"></i>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                        <button type="submit" class="btn btn-primary">Register</button>
+
+                        <!-- Remember me -->
+                        <div class="form-group form-check">
+                            <input
+                                type="checkbox"
+                                class="form-check-input"
+                                id="remember"
+                                name="remember"
+                                <?php if(isset($_COOKIE['username'])) echo "checked"; ?>
+                            >
+                            <label class="form-check-label" for="remember">Remember me</label>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
                     </form>
+
+                    <p class="mt-3 text-center">
+                        Don't have an account ? <a href="register.php">Register</a>
+                    </p>
+
                 </div>
             </div>
         </div>
     </div>
 
-<!-- Show password script -->
-<script>
-function togglePassword() {
-  var x = document.getElementsByName("password")[0];
-  x.type = (x.type === "password") ? "text" : "password";
-}
-</script>
+    <!-- JS để toggle mật khẩu -->
+    <script>
+    function togglePassword() {
+        let input = document.getElementById("password");
+        let icon  = document.getElementById("eye-icon");
+
+        if (input.type === "password") {
+            input.type = "text";
+            icon.classList.remove("fa-eye");
+            icon.classList.add("fa-eye-slash");
+        } else {
+            input.type = "password";
+            icon.classList.remove("fa-eye-slash");
+            icon.classList.add("fa-eye");
+        }
+    }
+    </script>
 
 </body>
 </html>
